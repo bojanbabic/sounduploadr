a714fb72e670602474bafaacb2b61555b7d06815
Bojan Babic <gbabun@gmail.com>
2011-02-17 01:10:28 +0000

    working version

--- upload.py
+++ upload.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import os, time, logging, random
+import os, time, logging, random, urllib
 
 import tornado.httpserver
 import tornado.ioloop
@@ -12,29 +12,28 @@ AWS_ACCESS_KEY='AKIAJ5TKKYN3Q7BPXCBQ'
 AWS_SECRET_KEY='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'
 BUCKET_NAME='soundcloudr'
 BUCKET_URI='https://s3-eu-west-1.amazonaws.com/soundcloudr/'
-TMP_FILE_STORAGE='/home/ec2-user/sounduploadr/tmp/'
+#TMP_FILE_STORAGE='/home/ec2-user/sounduploadr/tmp/'
 
 class MainHandler(tornado.web.RequestHandler):
         def get(self):
                 self.set_header('Content-Type', 'text/html')
                 #self.render("templates/upload.html.cp.1", title="Uplaod test" , data= [])
-                self.render("index.html", title="Uplaod test" , data= [])
+                self.render("index.html", title="Upload test" , data= [])
 class SendText(tornado.web.RequestHandler):
         def get(self):
-                title = self.get_argument('title', 'burning up inside')
-                signal = self.get_argument('signal', default=None)
-                self.write('title:%s<br>' % title)
-                self.write('signal:%s<br>' % signal)
-                filename='test.png'
+                title = tornado.escape.utf8(self.get_argument('title', None))
+                progressID = self.get_argument('X-Progress-ID', None)
+                s3File = Util.transferS3FromString(title, progressID)
 
-                u = Util()
+		hashProgressID = Util.getHash(progressID)
+		fileUrl = BUCKET_URI + 'file_'+ hashProgressID
+		titleUrl = BUCKET_URI + 'title_'+ hashProgressID
+                self.write('title:%s<br>' % title)
                 
-                fileNameS3=u.transferS3FromFile(filename, TMP_FILE_STORAGE)
-                titleNameS3=u.transferS3FromString(title)
-                fileUrl = BUCKET_URI+fileNameS3
-                titleUrl = BUCKET_URI+titleNameS3
-                if fileNameS3 is not None:
+                if s3File is not None:
+			self.write("File saved:")
                         self.write("<a href=\"%s\">%s</a><br>" %(fileUrl ,fileUrl))
+			self.write("Title '%s' saved:" % title)
                         self.write("<a href=\"%s\">%s</a>" %(titleUrl,titleUrl))
                 else:
                         self.write('hm something got wrong with file transfer')
@@ -43,50 +42,62 @@ class UploadHandler(tornado.web.RequestHandler):
         def post(self):
                 title = self.get_argument('title', default=None)
                 self.set_header('Content-Type', 'text/html')
-                title = self.get_argument('title', 'burning up inside')
                 progressID = self.get_argument('X-Progress-ID' , default=None)
                 filename = self.get_argument('media_file.name', default=None)
                 path = self.get_argument('media_file.path', default=None)
                 size = self.get_argument('media_file.size', default=None)
-                signal = self.get_argument('signal', default=None)
+
+
+                #fileNameS3=u.transferS3FromFile(path, TMP_FILE_STORAGE, progressID)
+                fileNameS3=Util.transferS3FromFile(path, progressID)
+                fileUrl = BUCKET_URI+fileNameS3
+
                 self.write('message:%s<br>' % title)
+                logging.error('message:%s<br>' % title)
                 self.write('filename %s<br>' % filename)
-                self.write('path %s<br>' % path)
-                self.write('progressID%s<br>' % progressID)
+                logging.error('filename %s<br>' % filename)
+                self.write('path:%s<br>' % path)
+                logging.error('path:%s<br>' % path)
+                self.write('progressID:%s<br>' % progressID)
+                logging.error('progressID:%s<br>' % progressID)
                 self.write('size %s<br>' % size) 
+                logging.error('size %s<br>' % size) 
 
                
 class Util:
         @staticmethod
-        def getHash():
-                now_str = str(int(time.time()))
-                rnd_str = str(random.randrange(0,1000))
-
-                hash_str = str(hash(str(now_str + '_' + rnd_str )))
+        def getHash(string):
+                hash_str = str(hash(string))
                 return hash_str[1:]
 
-        def savefile(self, filename, body):
+        @staticmethod
+        def savefile(filename, body):
                 fn = os.path.basename("%s_%s" %(filename,str(int(time.time()))))
                 open('files/'+ fn, 'wb').write(body)
                 #logging.info('file %s have been uploaded to server' % on)
 
-        def transferS3FromFile(self, filename, filedir):
-                k = self.getBucketKey()
-                k.key=filename+'_'+Util.getHash()
-                k.set_contents_from_filename(filedir+'/'+filename)
+        #def transferS3FromFile(self, filename, filedir, progressID):
+        @staticmethod
+        def transferS3FromFile(path, progressID):
+                k = Util.getBucketKey()
+                k.key='file_'+Util.getHash(progressID)
+                k.set_contents_from_filename(path)
                 k.set_acl('public-read')
 
                 return k.key
 
-        def transferS3FromString(self,title):
-                k = self.getBucketKey()
-                k.key='title_'+Util.getHash()
+        @staticmethod
+        def transferS3FromString(title, progressID):
+                k = Util.getBucketKey()
+		hash_string = Util.getHash(progressID)
+                k.key='title_'+ hash_string
                 k.set_contents_from_string(title)
                 k.set_acl('public-read')
 
                 return k.key
 
-        def getBucketKey(self):
+        @staticmethod
+        def getBucketKey():
                 from boto.s3.connection import S3Connection
                 conn=S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
                 b = conn.get_bucket(BUCKET_NAME)
