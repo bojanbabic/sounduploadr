12ea856d053bbd068eddcc8eb0c5050d3cf07251
Bojan Babic <gbabun@gmail.com>
2011-02-18 00:06:23 +0000

    some upgrades

--- upload.py
+++ upload.py
@@ -8,6 +8,9 @@ import tornado.web
 from hashlib import sha1
 from hmac import new as hmac
 
+from httplib import HTTP
+from urlparse import urlparse
+
 AWS_ACCESS_KEY='AKIAJ5TKKYN3Q7BPXCBQ'
 AWS_SECRET_KEY='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'
 BUCKET_NAME='soundcloudr'
@@ -21,22 +24,30 @@ class MainHandler(tornado.web.RequestHandler):
                 self.render("index.html", title="Upload test" , data= [])
 class SendText(tornado.web.RequestHandler):
         def get(self):
-                title = tornado.escape.utf8(self.get_argument('title', None))
+                #title = tornado.escape.utf8(self.get_argument('title', None))
+                title = unicode(self.get_argument('title', ''))
                 progressID = self.get_argument('X-Progress-ID', None)
-                s3File = Util.transferS3FromString(title, progressID)
 
+		s3File = None
+
+		if progressID is None:
+			logging.info('Progress Id is missing')
+			self.write('Request can\'t be proceesed without progressID')
+			return
 		hashProgressID = Util.getHash(progressID)
-		fileUrl = BUCKET_URI + 'file_'+ hashProgressID
-		titleUrl = BUCKET_URI + 'title_'+ hashProgressID
-                self.write('title:%s<br>' % title)
-                
-                if s3File is not None:
-			self.write("File saved:")
-                        self.write("<a href=\"%s\">%s</a><br>" %(fileUrl ,fileUrl))
-			self.write("Title '%s' saved:" % title)
+
+		if len(title) > 0:
+                	s3File = Util.transferS3FromString(title, progressID)
+			titleUrl = BUCKET_URI + 'title_'+ hashProgressID
+			self.write("Title '%s' saved in file on location:" % title)
                         self.write("<a href=\"%s\">%s</a>" %(titleUrl,titleUrl))
                 else:
-                        self.write('hm something got wrong with file transfer')
+                        self.write('hm something got wrong with title data transfer: no data<br>')
+
+                fileUrl = BUCKET_URI + 'file_'+ hashProgressID
+
+                self.write("File saved:")
+                self.write("<a href=\"%s\">%s</a><br>" %(fileUrl ,fileUrl))
 
 class UploadHandler(tornado.web.RequestHandler):
         def post(self):
@@ -47,6 +58,10 @@ class UploadHandler(tornado.web.RequestHandler):
                 path = self.get_argument('media_file.path', default=None)
                 size = self.get_argument('media_file.size', default=None)
 
+
+		if path is None:
+			logging.info('No file uploaded')
+			return
 
                 #fileNameS3=u.transferS3FromFile(path, TMP_FILE_STORAGE, progressID)
                 fileNameS3=Util.transferS3FromFile(path, progressID)
@@ -104,9 +119,15 @@ class Util:
                 from boto.s3.key import Key
                 k = Key(b)
                 return k
-
-               
-
+	@staticmethod
+	def checkUrl(url):
+		p = urlparse(url)
+		h = HTTP(p[1])
+		h.putrequest('HEAD', p[2])
+		h.endheaders()
+		if h.getreply()[0] != 404:
+			return True
+		return False
 
 settings = {'static_path': os.path.join(os.path.dirname(__file__), "static")}
 application=tornado.web.Application([
